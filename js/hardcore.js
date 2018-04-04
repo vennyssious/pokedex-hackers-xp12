02f6851095868d904eaf740e3d89d33ee7438990
littlecouto17 <vinicius.coutinho.sou@gmail.com>
2018-04-03 23:59:58 -0300

    Exportar

--- js/hardcore.js
+++ js/hardcore.js
@@ -54,3 +54,51 @@ function favoritar (id) {
   pokemon.add(id);
   return true;
 }
+
+function exportarCSV (pokemon){
+  let CSVData = pokemon.reduce((value, entry) =>
+    `${value}\r\n${entry.name},${entry.sprites.front_default},${entry.weight},${entry.height},${entry.abilities[0].ability.name},${entry.abilities[1].ability.name}`,
+    'pokename, photo, weight, height, ability1, ability2'
+  );
+  console.log(CSVData )
+  var a = document.createElement('A');
+  a.href = `data:text/csv;charset=UTF-8,${encodeURI(CSVData)}`;
+  a.download = "lista.csv";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+async function exportaFavoritos (){
+
+  if(window.exporting)
+    return false;
+
+  window.exporting = true;
+
+  let pokemons = [];
+
+  if(pokemon.data.length > 0){
+
+    await pokemon.data.map((poke, i) => {
+
+      let xhr = new XMLHttpRequest();
+      xhr.open("GET", `https://pokeapi.co/api/v2/pokemon/${poke}`);
+      xhr.onload = e => {
+        pokemons.push(JSON.parse(e.target.response));
+        if(pokemons.length >= pokemon.data.length){
+          exportarCSV(pokemons);
+          window.exporting = false;
+        }
+      }
+
+      xhr.send();
+
+    });
+
+
+
+  }
+
+
+}
